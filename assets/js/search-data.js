74aa82b098451707284e1fac36338e2bcf6de3a9
7point1 <102340698+7point1@users.noreply.github.com>
2026-01-25 05:28:14 +0000

    Deploying to gh-pages from @ 7point1/7point1.github.io@1db7905b89aee1fba5cbc7ea8ee69f6a59a49be9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "projects-1918-bundaberg",
           title: '1918 Bundaberg',
-          description: "Macroseismic and seismological review of the 6 June 1918 Bundaberg earthquake",
+          description: "Macroseismic and seismological review of the 6 June 1918 Bundaberg earthquake in Queensland, Australia",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
