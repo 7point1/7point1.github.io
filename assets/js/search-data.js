796f31478661743a9295d62d7461063ebb0219e7
7point1 <102340698+7point1@users.noreply.github.com>
2026-01-27 06:49:20 +0000

    Deploying to gh-pages from @ 7point1/7point1.github.io@cef7e4ce0a3d6b0d813ef2771f4879e6e307eaa7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -35,6 +35,31 @@ ninja.data = [{
           description: "Macroseismic and seismological review of the 6 June 1918 Bundaberg earthquake in Queensland, Australia",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
+            },},{id: "projects-1860-haiti",
+          title: '1860 Haiti',
+          description: "Macroseismic reanalysis of the three Jour de Pâques earthquakes on 8-9 April 1860 in southern Haiti",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/2_project/";
+            },},{id: "projects-1907-sumatra",
+          title: '1907 Sumatra',
+          description: "Upgrading the magnitude of the 4 January 1907 tsunami earthquake in Indonesia and the Indian Ocean",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/3_project/";
+            },},{id: "projects-1922-malaysia",
+          title: '1922 Malaysia',
+          description: "Magnitudes and location estimates for two rare earthquakes in Malaysia and Singapore in Jan-Feb 1922.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/4_project/";
+            },},{id: "projects-1954-adelaide",
+          title: '1954 Adelaide',
+          description: "Revision of the location of the 1 March 1954 Adelaide earthquake in South Australia",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
+            },},{id: "projects-1895-charleston",
+          title: '1895 Charleston',
+          description: "Using macroseismic data and observations to determine the true size of the 31 OCtober 1895 Charleston, Missouri, earthquake in the United States",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/6_project/";
             },},{
       id: 'light-theme',
       title: 'Change theme to light',
